647b4f1d9d79fa5a63b0c5cced91e56c17026642
Ewoma Ozore <ewomaozore@gmail.com>
2024-04-26 01:33:10 +0100

    designed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <footer className="bg-gray-800 text-white py-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div>
+                    <p className="text-sm">&copy; 2024 SpaceX. All rights reserved.</p>
+                </div>
+                <div>
+                    <ul className="flex space-x-4">
+                        <li><a href="/contact" className="text-sm hover:text-gray-300">About</a></li>
+                        <li><a href="/sevice" className="text-sm hover:text-gray-300">Services</a></li>
+                        <li><a href="/contact" className="text-sm hover:text-gray-300">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
