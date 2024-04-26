0ddf38bad95a11ed3d96e8972513c6c560e57ed9
Ewoma Ozore <ewomaozore@gmail.com>
2024-04-26 10:51:29 +0100

    fixed responsiveness

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const SearchForm: React.FC<{ onSubmit: (formData: { rocket_name:string; status: string; original_Launch: string; type: string }) => void }> = ({ onSubmit }) => {
+const SearchForm: React.FC<{ onSubmit: (formData: { rocket_name: string; status: string; original_Launch: string; type: string }) => void }> = ({ onSubmit }) => {
     const [status, setStatus] = useState('');
     const [original_Launch, setOriginal_Launch] = useState('');
     // eslint-disable-next-line
@@ -14,15 +14,15 @@ const SearchForm: React.FC<{ onSubmit: (formData: { rocket_name:string; status: 
 
     return (
         <div className="container mx-auto py-4">
-            <form className="flex justify-center" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Search by status" className="px-4 py-2 mr-2" value={status} onChange={(e) => setStatus(e.target.value)} />
-                <input type="date" placeholder="Original launch" className="px-4 py-2 mr-2" value={original_Launch} onChange={(e) => setOriginal_Launch(e.target.value)} />
-                <select className="px-4 py-2" value={type} onChange={(e) => setType(e.target.value)}>
+            <form className="flex flex-col md:flex-row justify-center items-center" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Search by status" className="px-4 py-2 mb-2 md:mb-0 md:mr-2" value={status} onChange={(e) => setStatus(e.target.value)} />
+                <input type="date" placeholder="Original launch" className="px-4 py-2 mb-2 md:mb-0 md:mr-2" value={original_Launch} onChange={(e) => setOriginal_Launch(e.target.value)} />
+                <select className="px-4 py-2 mb-2 md:mb-0 md:mr-2" value={type} onChange={(e) => setType(e.target.value)}>
                     <option value="">Select type</option>
                     <option value="rocket">Rocket</option>
                     <option value="capsule">Capsule</option>
                 </select>
-                <button type="submit" className="bg-blue-500 text-white px-4 py-2 ml-2">Search</button>
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2">Search</button>
             </form>
         </div>
     );
